10207af63e6ba0b9207dde2b6cc6504b852edc05
nvthienhao <55606328+nvthienhao@users.noreply.github.com>
2021-01-30 18:41:30 -0800

    SWAP

--- FPGAmeBoy.srcs/sources_1/new/ALU.sv
+++ FPGAmeBoy.srcs/sources_1/new/ALU.sv
@@ -42,16 +42,13 @@ module ALU(ALU_FUN, A, B, FLAGS_IN, ALU_OUT, FLAGS_OUT);
        localparam CPL  = 5'b01100;
        localparam CFF  = 5'b01101;
        localparam SCF  = 5'b01110;
-       localparam RLC  = 5'b01111;
-       localparam RL   = 5'b10000;
-       localparam RRC  = 5'b10001;
-       localparam RR   = 5'b10010;
+       localparam RLC = 5'b01111;
+       localparam RL  = 5'b10000;
+       localparam RRC = 5'b10001;
+       localparam RR  = 5'b10010;
        localparam SLA  = 5'b10011;
-       localparam SRA  = 5'b10100;
+       localparam SRA   = 5'b10100;
        localparam SRL  = 5'b10101;
-       localparam BIT  = 5'b10110;
-       localparam SET  = 5'b10111;
-       localparam RES  = 5'b11000;
 
        
        // Flag Register Bits
@@ -255,6 +252,38 @@ module ALU(ALU_FUN, A, B, FLAGS_IN, ALU_OUT, FLAGS_OUT);
                         else           
                             FLAGS_OUT[Z_FLAG] = 1'b0;   
                     end 
+                    
+                  //Swap upper and lower nibbles of input A
+                  SWAP:
+                    begin
+                        //Set Z flag to 1 if result is zero
+                        FLAGS_OUT[Z_FLAG] = (ALU_OUT   == 8'b0) ? 1'b1 : 1'b0;
+                        //Set N,H,C flags to zero
+                        FLAGS_OUT[H_FLAG] = 1'b0; 
+                        FLAGS_OUT[N_FLAG] = 1'b0; 
+                        FLAGS_OUT[C_FLAG] = 1'b0; 
+                        ALU_OUT = {A[3:0], A[7:4]};
+                    end 
+                    
+                  // DAA - decimal adjust after addition
+                  // Used to correct presentation of ALU_OUT to BCD
+                  DAA:
+                    begin
+                        //Set Z flag to 1 if result is zero
+                        FLAGS_OUT[Z_FLAG] = (ALU_OUT   == 8'b0) ? 1'b1 : 1'b0;
+                        //N flag is not affected
+                        FLAGS_OUT[N_FLAG] = FLAGS_IN[N_FLAG];
+                        // H flag is set to 0
+                        FLAGS_OUT[H_FLAG] = 1'b0;
+                        // for ADD case
+                        if(~FLAGS_IN[N_FLAG])
+                            begin
+                                //if there was a borrow (Half-carry flag equals 1) or the lower nibble was greater than 9
+                                //add 6 to the result
+                                
+                            end
+                    end
+                    
                 // Used for the C and CB Prefix commands  
                 // Rotates A input left, MSB = Carry Flag 
                 // LSB = A input MSB      
@@ -336,98 +365,9 @@ module ALU(ALU_FUN, A, B, FLAGS_IN, ALU_OUT, FLAGS_OUT);
                             FLAGS_OUT[Z_FLAG] = 1'b1;
                         else           
                             FLAGS_OUT[Z_FLAG] = 1'b0;   
-                    end
-                // Shifts A input by 1 to the left into the Carry Flag
-                // LSB = 0  
-                SLA:
-                    begin
-                        // Resets the Subtract FLag
-                        FLAGS_OUT[N_FLAG] = 1'b0;
-                        // Resets the Half Carry FLag
-                        FLAGS_OUT[H_FLAG] = 1'b0;
-                        // Shifts the bits in A to the left by 1
-                        ALU_OUT[7:1] = A[6:0];
-                        ALU_OUT[0] = 0;
-                        // Sets the Carry Flag equal to the MSB of A
-                        FLAGS_OUT[C_FLAG] = A[7];
-                        // Z Flag conditional
-                        if (ALU_OUT == 8'b0)
-                            FLAGS_OUT[Z_FLAG] = 1'b1;
-                        else           
-                            FLAGS_OUT[Z_FLAG] = 1'b0;   
-                    end
-                // Shifts A input by 1 to the right into the Carry Flag
-                // MSB is not changed
-                SRA:
-                    begin
-                        // Resets the Subtract FLag
-                        FLAGS_OUT[N_FLAG] = 1'b0;
-                        // Resets the Half Carry FLag
-                        FLAGS_OUT[H_FLAG] = 1'b0;
-                        // Shifts the bits in A to the right by 1, except MSB
-                        ALU_OUT[6:0] = A[7:1];
-                        ALU_OUT[7] = A[7];
-                        // Sets the Carry Flag equal to the LSB of A
-                        FLAGS_OUT[C_FLAG] = A[0];
-                        // Z Flag conditional
-                        if (ALU_OUT == 8'b0)
-                            FLAGS_OUT[Z_FLAG] = 1'b1;
-                        else           
-                            FLAGS_OUT[Z_FLAG] = 1'b0;   
-                    end
-                  // Shifts A input by 1 to the left into the Carry Flag
-                  // MSB = 0
-                  SRL:
-                    begin
-                        // Resets the Subtract FLag
-                        FLAGS_OUT[N_FLAG] = 1'b0;
-                        // Resets the Half Carry FLag
-                        FLAGS_OUT[H_FLAG] = 1'b0;
-                        // Shifts the bits in A to the right by 1, MSB = 0
-                        ALU_OUT[6:0] = A[7:1];
-                        ALU_OUT[7] = 0;
-                        // Sets the Carry Flag equal to the LSB of A
-                        FLAGS_OUT[C_FLAG] = A[0];
-                        // Z Flag conditional
-                        if (ALU_OUT == 8'b0)
-                            FLAGS_OUT[Z_FLAG] = 1'b1;
-                        else           
-                            FLAGS_OUT[Z_FLAG] = 1'b0;   
-                    end 
-                    // Test bit [input B: 0-7] of input register [input A]
-                    BIT:
-                        begin
-                            // Resets the Subtract FLag
-                            FLAGS_OUT[N_FLAG] = 1'b0;
-                            // Sets the Half Carry FLag
-                            FLAGS_OUT[H_FLAG] = 1'b0;
-                            // No Change in the Carry Flag
-                            FLAGS_OUT[C_FLAG] = FLAGS_IN[C_FLAG];
-                            // Z Flag set if bit is 0, it is cleared otherwise                            
-                            FLAGS_OUT[Z_FLAG] = ~A[B]; 
-                            // Outputs index bit 
-                            ALU_OUT = B;                                               
-                        end
-                    // Set bit [input B: 0-7] of input register [input A]
-                    SET:
-                        begin
-                           // No change in Flags 
-                           FLAGS_OUT = FLAGS_IN;
-                           // Sets the output equal to A
-                           ALU_OUT = A;
-                           // Sets the specified bit
-                           ALU_OUT[B] = 1'b1; 
-                        end
-                    // Clears bit [input B: 0-7] of input register [input A]
-                    RES:
-                        begin
-                           // No change in Flags 
-                           FLAGS_OUT = FLAGS_IN;
-                           // Sets the output equal to A
-                           ALU_OUT = A;
-                           // Clears the specified bit
-                           ALU_OUT[B] = 1'b0; 
-                        end                                 
+                    end  
+  
+                                                      
                 default: 
                     begin
                         ALU_OUT = 8'b0;
