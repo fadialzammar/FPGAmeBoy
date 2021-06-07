8b201406cc3d33432fc01636ffe53764516a4211
nvthienhao <55606328+nvthienhao@users.noreply.github.com>
2021-06-06 23:05:45 -0700

    mbc1
    not tested yet

--- FPGAmeBoy.srcs/sources_1/new/ProgRom.sv
+++ FPGAmeBoy.srcs/sources_1/new/ProgRom.sv
@@ -25,11 +25,15 @@ module ProgRom(
     input PROG_CLK,
     input RE,
     input [15:0] PROG_ADDR,
+    input [20:0] MBC_ADDR,
     output logic [7:0] PROG_IR,
     
     input [15:0] CART_ADDR,
     input CART_RE,
-    output logic [7:0] CART_DATA
+    output logic [7:0] CART_DATA,
+    
+    output logic [2:0] RAM_BANKS,
+    output logic [7:0] ROM_BANKS
     );
       
     (* rom_style="{distributed | block}" *) // force the ROM to be block memory
@@ -44,7 +48,41 @@ module ProgRom(
     always_ff @(posedge PROG_CLK)
     begin
         //if (RE)
-            PROG_IR <= rom[PROG_ADDR];
+            //PROG_IR <= rom[PROG_ADDR];
+            PROG_IR <= rom[MBC_ADDR];
             CART_DATA <= rom[CART_ADDR];
+
+    end
+    
+    always_comb 
+    begin
+    //check ROM size    
+        if ( PROG_ADDR == 328 ) begin // 148 in hex
+            if( rom[PROG_ADDR] == 0 )
+                ROM_BANKS = 0; // 32kB, no ROM banking
+            else if ( rom[PROG_ADDR] == 1 )
+                ROM_BANKS = 4; // 64kB, 4 banks
+            else if ( rom[PROG_ADDR] == 2 )
+                ROM_BANKS = 8; // 128kB, 8 banks
+            else if ( rom[PROG_ADDR] == 3 )
+                ROM_BANKS = 16; // 256kB, 16 banks
+            else if ( rom[PROG_ADDR] == 4 )
+                ROM_BANKS = 32; // 512kB, 32 banks
+            else if ( rom[PROG_ADDR] == 5 )
+                ROM_BANKS = 64; // 1MB, 64 banks
+            else if ( rom[PROG_ADDR] == 6 )
+                ROM_BANKS = 128; // ~2MB, 128 banks
+        end
+        //check RAM size
+        if ( PROG_ADDR == 329 ) begin // 149 in hex
+            if( rom[PROG_ADDR] == 0 )
+                RAM_BANKS = 0; // no RAM
+            else if ( rom[PROG_ADDR] == 1 )
+                RAM_BANKS = 0; // 2kB - unofficial
+            else if ( rom[PROG_ADDR] == 2 )
+                RAM_BANKS = 1; // 8 kB, 1 bank
+            else if ( rom[PROG_ADDR] == 3 )
+                RAM_BANKS = 4; // 32kB, 4 banks of 8kB each
+        end
     end
 endmodule
---
+++ FPGAmeBoy.srcs/sources_1/new/mbc1.sv
@@ -0,0 +1,102 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/23/2021 07:24:00 PM
+// Design Name: 
+// Module Name: mbc1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module mbc1(
+    input CLK,
+    input [15:0] ADDR,
+    input [7:0] DATA,
+    input WR,
+    input RD,
+    input RST,
+    input [7:0] ROM_BANKS,
+    input [2:0] RAM_BANKS,
+    
+    output logic [20:0] MBC_ADDR,
+    output logic [7:0] MBC_DATA
+    );
+    
+    
+    //logic m_CurrentROMBank = 1;
+    logic [6:0] bank_num; //selected Rom bank = bank_num[6:5] + bank_num[4:0]
+    logic [1:0] ram_bank;
+    logic banking_mode = 0; // default 0 ROM banking, set to 1 for RAM banking
+    logic ram_en ;
+    //reg [ram 
+    
+    always_ff @(posedge CLK)
+    begin
+        if(RST) begin
+            ram_en <= 0;
+            bank_num <= 7'b0;
+            ram_bank <= 2'b0;
+            banking_mode <= 0; 
+        end 
+       
+    end
+    
+    always_comb
+    begin
+        MBC_DATA = DATA;
+        // access to bank 00, 20, 40 and 60 is not allowed
+        // automatically increment bank_num by 1 
+        if ( bank_num == 7'h00 || bank_num == 7'h20 || bank_num == 7'h40 || bank_num == 7'h60 )
+            bank_num += 1;
+            
+        if( RD ) begin
+            if( ADDR < 16'h4000 ) begin //READ ROM Bank 0 or 20,40 & 60 when baking mode is set to 1
+                if(banking_mode)
+                    MBC_ADDR <= ADDR + (bank_num[6:5]<<19);//Bank 0, 20, 40, 60
+                    // 
+                else 
+                    MBC_ADDR <= {5'b0,ADDR}; // Bank 0 only
+            end
+            else if ( ADDR >= 16'h4000 && ADDR < 16'h8000 ) begin //Read ROM bank from 01-7F, each has 16kB
+                MBC_ADDR <= (ADDR-16'h4000)  + (bank_num[6:0] << 14)  ;
+            end
+            else if ( ADDR >= 16'hA000 && ADDR < 16'hC000 && ram_en ) begin //Read Ram bank 00-03 if any
+                MBC_ADDR <= ram_bank[1:0]<<16 + ADDR;
+                
+            end
+        end
+        
+        if( WR ) begin
+            if( ADDR < 16'h2000 ) // enable RAM
+                ram_en <= (DATA[3:0] == 4'hA) ? 1'b1 : 1'b0;
+            else if( ADDR >= 16'h2000 && ADDR < 16'h4000) //select ROM bank number from $01-$1F
+                bank_num[4:0] <= DATA[4:0];
+            else if( ADDR >= 16'h4000 && ADDR < 16'h6000) begin
+                if(banking_mode && ram_en) // select RAM bank
+                    ram_bank[1:0] <= DATA[1:0];
+                else    //select 2 upper bits of ROM bank
+                    bank_num[6:5] <= DATA[1:0];
+            end
+            else if( ADDR >= 16'h6000 && ADDR < 16'h8000) begin
+                if( ROM_BANKS > 8'd16 || RAM_BANKS > 3'd1) 
+                    banking_mode = 1;
+                //else if ( )
+                    //banking_mode=1;
+                else banking_mode = 0;  
+            end
+        end
+    end
+        
+endmodule
