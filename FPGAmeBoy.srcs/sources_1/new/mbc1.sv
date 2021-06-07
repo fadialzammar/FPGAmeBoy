4cf43e2374a2f9e7cd1dd830165db59b554049a2
nvthienhao <55606328+nvthienhao@users.noreply.github.com>
2021-06-06 23:09:53 -0700

    mbc1.0.1

--- FPGAmeBoy.srcs/sources_1/new/mbc1.sv
+++ FPGAmeBoy.srcs/sources_1/new/mbc1.sv
@@ -64,30 +64,30 @@ module mbc1(
         if( RD ) begin
             if( ADDR < 16'h4000 ) begin //READ ROM Bank 0 or 20,40 & 60 when baking mode is set to 1
                 if(banking_mode)
-                    MBC_ADDR <= ADDR + (bank_num[6:5]<<19);//Bank 0, 20, 40, 60
+                    MBC_ADDR = ADDR + (bank_num[6:5]<<19);//Bank 0, 20, 40, 60
                     // 
                 else 
-                    MBC_ADDR <= {5'b0,ADDR}; // Bank 0 only
+                    MBC_ADDR = {5'b0,ADDR}; // Bank 0 only
             end
             else if ( ADDR >= 16'h4000 && ADDR < 16'h8000 ) begin //Read ROM bank from 01-7F, each has 16kB
-                MBC_ADDR <= (ADDR-16'h4000)  + (bank_num[6:0] << 14)  ;
+                MBC_ADDR = (ADDR-16'h4000)  + (bank_num[6:0] << 14)  ;
             end
             else if ( ADDR >= 16'hA000 && ADDR < 16'hC000 && ram_en ) begin //Read Ram bank 00-03 if any
-                MBC_ADDR <= ram_bank[1:0]<<16 + ADDR;
+                MBC_ADDR = ram_bank[1:0]<<16 + ADDR;
                 
             end
         end
         
         if( WR ) begin
             if( ADDR < 16'h2000 ) // enable RAM
-                ram_en <= (DATA[3:0] == 4'hA) ? 1'b1 : 1'b0;
+                ram_en = (DATA[3:0] == 4'hA) ? 1'b1 : 1'b0;
             else if( ADDR >= 16'h2000 && ADDR < 16'h4000) //select ROM bank number from $01-$1F
-                bank_num[4:0] <= DATA[4:0];
+                bank_num[4:0] = DATA[4:0];
             else if( ADDR >= 16'h4000 && ADDR < 16'h6000) begin
                 if(banking_mode && ram_en) // select RAM bank
-                    ram_bank[1:0] <= DATA[1:0];
+                    ram_bank[1:0] = DATA[1:0];
                 else    //select 2 upper bits of ROM bank
-                    bank_num[6:5] <= DATA[1:0];
+                    bank_num[6:5] = DATA[1:0];
             end
             else if( ADDR >= 16'h6000 && ADDR < 16'h8000) begin
                 if( ROM_BANKS > 8'd16 || RAM_BANKS > 3'd1) 
